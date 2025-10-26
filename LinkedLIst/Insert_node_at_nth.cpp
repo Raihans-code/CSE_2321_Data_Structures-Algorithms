5088f66a87d851bb4669860ec73c0f3a5400332c
Raihan Islam <iraihan705@gmail.com>
2025-10-26 22:24:39 +0600

    Fixed linked list

--- LinkedLIst/Insert_node_at_nth.cpp
+++ LinkedLIst/Insert_node_at_nth.cpp
@@ -1,82 +1,91 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
+
 class Node
 {
-  public:
+public:
   int info;
-  Node* link;
+  Node *link;
   Node(int info)
   {
-    this-> info = info;
-    this-> link = NULL; 
+    this->info = info;
+    this->link = NULL;
   }
 };
-void Insert_at_position(Node*& start,int val,int pos)
+
+void print_list(Node *ptr)
 {
-  Node *ptr = new Node(val);// set info[ptr] = val and link[ptr] = NULL
-  if(pos==0)// IF position is 0
+  while (ptr != NULL)
   {
-    if(start==NULL)// IF list is Start is NULL
-    {
-      start = ptr;
-    }
-    else // ELSE link[ptr] = start and start = ptr
-    {
-      ptr->link = start;
-      start=ptr;
-    }
+    cout << ptr->info << "->";
+    ptr = ptr->link;
   }
-  else // ELSE traverse to the (pos-1)th node
+  cout << "NULL"<< endl;
+}
+
+void insert_at_nth(Node *start, int pos, int val)
+{
+  Node *newNode = new Node(val);
+
+  if (pos == 0)
   {
-    Node* ptr = start;
-    for(int i=0;i<pos-1 && ptr!=NULL;i++)
-    {
-      ptr=ptr->link;
-    }
-    if(ptr==NULL)// IF (pos-1)th node is NULL
-    {
-      cout << "Position out of bounds" << endl;
-      delete temp; // Free the allocated memory for the new node
-      return;
-    }
-    // ELSE link[new node] = link[(pos-1)th node] and link[(pos-1)th node] = new node
-    temp->link = ptr->link;
-    ptr->link = temp;
+    start = newNode;
+    return;
   }
+
+  Node *ptr = start;
+
+  for (int i = 1; i <=pos; ++i)
+  {
+    ptr =  ptr->link;
+  }
+  if(ptr == NULL)
+  {
+    cout << "Position out of BOUNDs!"<<endl;
+    delete newNode;
+    newNode = nullptr;
+    return;
+  }
+  else{
+    newNode->link = ptr->link;
+    ptr->link = newNode;
+  }
+
 }
 
 int main()
 {
   int n;
+  cout << "Enter Number Data:";
   cin >> n;
 
   Node *start = NULL;
 
-  for(int i = 0;i<n;++i)
+  cout << "Enter Data:";
+  for (int i = 0; i < n; ++i)
   {
     int val;
     cin >> val;
     Node *temp = new Node(val);
-    if(start==NULL)
+
+    if (start == NULL)
     {
       start = temp;
     }
     else
     {
       temp->link = start;
-      start=temp;
+      start = temp;
     }
   }
-  int value,position;
-  cin >> value >> position;
-  Insert_at_position(start,value,position-1);//Insert a node with value 'value' at position 'position'
-  // Print the linked list
-  Node* ptr = start;
-  while(ptr!=NULL)
-  {
-    cout << ptr->info << "->";
-    ptr = ptr->link;
-  }
-  cout<< "NULL" << endl;
-  return 0;
+  print_list(start);
+
+  int item,position;
+  cout << "Enter item:";
+  cin >> item;
+  cout << "Position";
+  cin >> position;
+  insert_at_nth(start,position-2,item);
+  print_list(start);
 }
